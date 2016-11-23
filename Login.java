bd384a454f0f5b71eb53e5e9f08a5b94e615bf32
AlphaMGK <septian.alfarizi.21@gmail.com>
2016-11-23 07:44:41 +0700

    Login

---
+++ Login.java
@@ -0,0 +1,18 @@
+package kel2.ddplc.inventoryfaris;
+
+/**
+ * Created by Alpha on 12/11/2016.
+ */
+
+public class Login {
+//    Database username = u;
+//    Database pass = p;
+
+    void confirmLogin(){
+
+    }
+
+    boolean aksesDitolak(){
+        return true;
+    }
+}
