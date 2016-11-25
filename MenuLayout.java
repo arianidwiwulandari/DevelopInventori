3593b95cecb6e307326cb91b600b7aa8039aeadf
arianidwiwulandari <Ariani_dwiwulandari@yahoo.com>
2016-11-25 15:07:17 +0700

    menu.java

---
+++ MenuLayout.java
@@ -0,0 +1,113 @@
+package kel2.ddplc.inventoryfaris;
+
+import android.content.Intent;
+import android.media.Image;
+import android.net.Uri;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.ImageButton;
+import android.view.View.OnClickListener;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+public class MenuLayout extends AppCompatActivity {
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
+    public View.OnClickListener btnProdukListener = new OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent i = new Intent(getApplicationContext(), MenuProdukLayout.class);
+            startActivity(i);
+        }
+    };
+    public OnClickListener btnRekabListener = new OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent i = new Intent(getApplicationContext(), RekapLayout.class);
+            startActivity(i);
+        }
+    };
+    public OnClickListener btnNotifListener = new OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent i = new Intent(getApplicationContext(), NotifLayout.class);
+            startActivity(i);
+        }
+    };
+    public OnClickListener btnStatistikListener = new OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent i = new Intent(getApplicationContext(), StatistikLayout.class);
+            startActivity(i);
+        }
+    };
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_layout);
+
+        ImageButton btnProduk = (ImageButton) findViewById(R.id.btnProduk);
+        btnProduk.setOnClickListener(btnProdukListener);
+        ImageButton btnRekab = (ImageButton) findViewById(R.id.btnRekab);
+        btnRekab.setOnClickListener(btnRekabListener);
+        ImageButton btnNotif = (ImageButton) findViewById(R.id.btnNotif);
+        btnNotif.setOnClickListener(btnNotifListener);
+        ImageButton btnStatistik = (ImageButton) findViewById(R.id.btnStatistik);
+        btnStatistik.setOnClickListener(btnStatistikListener);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+//    public boolean onCreateOptioMenu(Menu menu) {
+//        getMenuInflater().inflate(R.menu.activity_menu_layout, menu);
+//        return true;
+//    }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("MenuLayout Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
+    }
+}
