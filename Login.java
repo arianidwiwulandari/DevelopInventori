3dd079d8c7c7853af6c52bec4d9c6db4b8d6f105
AlphaMGK <septian.alfarizi.21@gmail.com>
2016-11-25 23:28:19 +0700

    editing

--- Login.java
+++ Login.java
@@ -4,10 +4,11 @@ package kel2.ddplc.inventoryfaris;
  * Created by Alpha on 12/11/2016.
  */
 
-public class Login {
-//    Database username = u;
-//    Database pass = p;
+class Login {
 
+    private static final String[] DUMMY_CREDENTIALS = new String[]{
+            "foo@example.com:hello", "bar@example.com:world"
+    };
     void confirmLogin(){
 
     }
