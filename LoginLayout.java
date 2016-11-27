5dc8499e994b289501624a6faeb65d28288a84e5
AlphaMGK <septian.alfarizi.21@gmail.com>
2016-11-28 04:25:58 +0700

    login

--- LoginLayout.java
+++ LoginLayout.java
@@ -3,38 +3,23 @@ package kel2.ddplc.inventoryfaris;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Handler;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.util.Patterns;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-/**
- * A login screen that offers login via email/password.
- */
 class LoginLayout extends AppCompatActivity {
 
-    /**
-     * Id to identity READ_CONTACTS permission request.
-     */
-
-    /**
-     * A dummy authentication store containing known user names and passwords.
-     * TODO: remove after connecting to a real authentication system.
-     */
     private static final String TAG = "Login";
-    private static final int REQUEST_SIGNUP = 0;
-
+    private Toast backtoast;
     // UI references.
     private EditText usernameView;
     private EditText passwordView;
     private Button btnLogin;
-    private View mProgressView;
-    private View mLoginFormView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,101 +28,97 @@ class LoginLayout extends AppCompatActivity {
         // Set up the login form.
         usernameView = (EditText) findViewById(R.id.txtusername);
         passwordView = (EditText) findViewById(R.id.txtpassword);
-        btnLogin = (Button) findViewById(R.id.btn_login);
-        btnLogin.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                login();
-            }
-        });
     }
 
-    private void aksesDitolak(){
+    public void login(View view) {
+        Profile user = new Profile();
+        String username = usernameView.getText().toString();
+        String password = passwordView.getText().toString();
+        //dummy credential
+        user.setUsername("admin");
+        user.setPassword("admin");
 
-    }
-    public void login() {
         Log.d(TAG, "Login");
-
         if (!validate()) {
-            onLoginFailed();
             return;
         }
 
         btnLogin.setEnabled(false);
 
-        final ProgressDialog progressDialog = new ProgressDialog(LoginLayout.this,
-                R.style.AppTheme_Dark_Dialog);
+        final ProgressDialog progressDialog = new ProgressDialog(LoginLayout.this);
         progressDialog.setIndeterminate(true);
-        progressDialog.setMessage("Authenticating...");
+        progressDialog.setMessage("Logging in...");
         progressDialog.show();
 
-        String email = usernameView.getText().toString();
-        String password = passwordView.getText().toString();
 
         // TODO: Implement your own authentication logic here.
-
-        new Handler().postDelayed(
-                new Runnable() {
-                    public void run() {
-                        // On complete call either onLoginSuccess or onLoginFailed
-                        onLoginSuccess();
-                        // onLoginFailed();
-                        progressDialog.dismiss();
-                    }
-                }, 3000);
-    }
-
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_SIGNUP) {
-            if (resultCode == RESULT_OK) {
-
-                // TODO: Implement successful signup logic here
-                // By default we just finish the Activity and log them in automatically
-                this.finish();
-            }
+        if (username.contains(user.getUsername()) && password.contains(user.getPassword())) {
+            new Handler().postDelayed(
+                    new Runnable() {
+                        public void run() {
+                            loginSukses();
+                            progressDialog.dismiss();
+                        }
+                    }, 3000);
+        } else {
+            new Handler().postDelayed(
+                    new Runnable() {
+                        public void run() {
+                            aksesDitolak();
+                            progressDialog.dismiss();
+                        }
+                    }, 3000);
         }
     }
 
+    long lastPress;
+    Toast backpressToast;
     @Override
     public void onBackPressed() {
-        // Disable going back to the MainActivity
-        moveTaskToBack(true);
+        long currentTime = System.currentTimeMillis();
+        if(currentTime - lastPress > 5000){
+            backpressToast = Toast.makeText(getBaseContext(), "Press back again to exit", Toast.LENGTH_LONG);
+            backpressToast.show();
+            lastPress = currentTime;
+        } else {
+            if (backpressToast != null)
+                backpressToast.cancel();
+            super.onBackPressed();
+        }
     }
 
-    public void onLoginSuccess() {
+    public void loginSukses() {
+        Intent intent = new Intent(this, MenuProdukLayout.class);
         btnLogin.setEnabled(true);
-        finish();
+        startActivity(intent);
+        this.finish();
     }
 
-    public void onLoginFailed() {
-        Toast.makeText(getBaseContext(), "Login failed", Toast.LENGTH_LONG).show();
-
+    public void aksesDitolak() {
+        Toast.makeText(getBaseContext(), "Gagal sign in. Username/Password salah", Toast.LENGTH_LONG).show();
         btnLogin.setEnabled(true);
     }
 
     public boolean validate() {
         boolean valid = true;
 
-        String email = usernameView.getText().toString();
+        String username = usernameView.getText().toString();
         String password = passwordView.getText().toString();
 
-        if (email.isEmpty() || !Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
-            usernameView.setError("enter a valid email address");
+        if (username.isEmpty()) {
+            usernameView.setError("masukkan username");
             valid = false;
         } else {
             usernameView.setError(null);
         }
 
-        if (password.isEmpty() || password.length() < 4 || password.length() > 10) {
-            passwordView.setError("between 4 and 10 alphanumeric characters");
+        if (password.isEmpty()) {
+            passwordView.setError("masukkan password");
             valid = false;
         } else {
             passwordView.setError(null);
         }
 
         return valid;
-    }
-    private void tampilkanPass(){
-
     }
 }
---
+++ Profile.java
@@ -0,0 +1,26 @@
+package kel2.ddplc.inventoryfaris;
+
+/**
+ * Created by Alpha on 25/11/2016.
+ */
+
+class Profile {
+    private String username;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
