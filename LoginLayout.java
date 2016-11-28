0042cf6b1a28e108bfe392f32f62ad012c0db797
AlphaMGK <septian.alfarizi.21@gmail.com>
2016-11-28 10:30:12 +0700

    edit

--- LoginLayout.java
+++ LoginLayout.java
@@ -2,13 +2,13 @@ package kel2.ddplc.inventoryfaris;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
-import android.os.Handler;
+import android.os.CountDownTimer;
 import android.support.design.widget.TextInputEditText;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -16,10 +16,10 @@ class LoginLayout extends AppCompatActivity {
 
     private static final String TAG = "Login";
     private Toast backtoast;
+    Profile user = new Profile();
     // UI references.
     private EditText usernameView;
-    private EditText passwordView;
-    private Button btnLogin;
+    private TextInputEditText passwordView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,23 +27,17 @@ class LoginLayout extends AppCompatActivity {
         setContentView(R.layout.login);
         // Set up the login form.
         usernameView = (EditText) findViewById(R.id.txtusername);
-        passwordView = (EditText) findViewById(R.id.txtpassword);
+        passwordView = (TextInputEditText) findViewById(R.id.txtpassword);
     }
 
     public void login(View view) {
-        Profile user = new Profile();
         String username = usernameView.getText().toString();
         String password = passwordView.getText().toString();
-        //dummy credential
-        user.setUsername("admin");
-        user.setPassword("admin");
 
         Log.d(TAG, "Login");
         if (!validate()) {
             return;
         }
-
-        btnLogin.setEnabled(false);
 
         final ProgressDialog progressDialog = new ProgressDialog(LoginLayout.this);
         progressDialog.setIndeterminate(true);
@@ -53,21 +47,29 @@ class LoginLayout extends AppCompatActivity {
 
         // TODO: Implement your own authentication logic here.
         if (username.contains(user.getUsername()) && password.contains(user.getPassword())) {
-            new Handler().postDelayed(
-                    new Runnable() {
-                        public void run() {
-                            loginSukses();
-                            progressDialog.dismiss();
-                        }
-                    }, 3000);
+            new CountDownTimer(2000, 1000) {
+
+                public void onTick(long millisUntilFinished) {
+                    // You don't need anything here
+                }
+
+                public void onFinish() {
+                    progressDialog.dismiss();
+                    loginSukses();
+                }
+            }.start();
         } else {
-            new Handler().postDelayed(
-                    new Runnable() {
-                        public void run() {
-                            aksesDitolak();
-                            progressDialog.dismiss();
-                        }
-                    }, 3000);
+            new CountDownTimer(2000, 1000) {
+
+                public void onTick(long millisUntilFinished) {
+                    // You don't need anything here
+                }
+
+                public void onFinish() {
+                    progressDialog.dismiss();
+                    aksesDitolak();
+                }
+            }.start();
         }
     }
 
@@ -88,15 +90,12 @@ class LoginLayout extends AppCompatActivity {
     }
 
     public void loginSukses() {
-        Intent intent = new Intent(this, MenuProdukLayout.class);
-        btnLogin.setEnabled(true);
+        Intent intent = new Intent(this, MenuLayout.class);
         startActivity(intent);
-        this.finish();
     }
 
     public void aksesDitolak() {
-        Toast.makeText(getBaseContext(), "Gagal sign in. Username/Password salah", Toast.LENGTH_LONG).show();
-        btnLogin.setEnabled(true);
+        Toast.makeText(getBaseContext(), "Gagal login. Username/Password salah", Toast.LENGTH_LONG).show();
     }
 
     public boolean validate() {
@@ -120,5 +119,10 @@ class LoginLayout extends AppCompatActivity {
         }
 
         return valid;
+    }
+    public void ubahPass(View view){
+        FragmentManager fm = getSupportFragmentManager();
+        ChangePassFragment changepass_DialogFragment = ChangePassFragment.newInstance("Some Title");
+        changepass_DialogFragment.show(fm, "ChangePassFragment");
     }
 }
