58b1ad5987e274c6ca2ccb14156dfce773fc36e9
arianidwiwulandari <Ariani_dwiwulandari@yahoo.com>
2016-12-04 17:13:04 +0700

    Add files via upload

---
+++ ListProduk.java
@@ -0,0 +1,32 @@
+package kel2.ddplc.inventoryfaris;
+
+import java.math.BigDecimal;
+import java.sql.ResultSet;
+
+/**
+ * Created by Alpha on 12/11/2016.
+ */
+
+public class ListProduk {
+    void updateList(){
+
+    }
+
+    void inputProduk(int kategori_produk, String namaproduk){
+
+    }
+
+    void updateStok(int IDproduk, int jumlah_produk){
+
+    }
+
+    void inputHarga(int IDproduk, BigDecimal harga){
+
+    }
+
+//    ResultSet getProductList(int kategori){
+//        return ResultSet;
+//    }
+
+
+}
