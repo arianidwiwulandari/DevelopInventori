faa16c368b156232bdb3aa24dde917ee1f973c0f
AlphaMGK <septian.alfarizi.21@gmail.com>
2016-12-13 13:31:31 +0700

    restructure so matches android's gradle framework

---
+++ app/src/androidTest/java/com/android/ui/SearchProductFragmentTest.java
@@ -0,0 +1,18 @@
+package com.android.ui;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by Alpha on 13/12/2016.
+ */
+public class SearchProductFragmentTest extends TestCase {
+
+    public void testShowData() throws Exception {
+
+    }
+
+    public void testLoadData() throws Exception {
+
+    }
+
+}
